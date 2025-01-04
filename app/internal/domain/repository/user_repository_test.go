e4d346bedf57981b47f260b6a244bc79297ebbff
nagisa599 <nagi46460510@icloud.com>
2025-01-04 18:36:52 +0900

    testの追加

---
+++ app/internal/domain/repository/user_repository_test.go
@@ -0,0 +1,94 @@
+package repository
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/nasunagisa/restapi/app/internal/domain"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func TestGetUser(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	mock.ExpectQuery("SELECT VERSION()").WillReturnRows(sqlmock.NewRows([]string{"version"}).AddRow("5.7.32"))
+
+	gormDB, err := gorm.Open(mysql.New(mysql.Config{
+		Conn: db,
+	}), &gorm.Config{})
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when initializing gorm", err)
+	}
+
+	repo := NewUserRepository(gormDB)
+
+	t.Run("正常系", func(t *testing.T) {
+		// テストデータを入れる
+		userId := int64(1)
+		expectedUsername := "testuser"
+		rows := sqlmock.NewRows([]string{"id", "username"}).AddRow(userId, expectedUsername)
+			
+		mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `user` WHERE id = ? ORDER BY `user`.`id` LIMIT ?")).
+		WithArgs(userId,1).  // id と LIMIT の値として 1 を期待
+		WillReturnRows(rows)
+
+
+		user := &domain.User{}
+		// 実行のエラーが出ないこと
+		err = repo.GetUser(userId, user)
+		assert.NoError(t, err, "Error was not expected while fetching user")
+		assert.Equal(t, expectedUsername, user.Name, "Expected username does not match actual username")
+		
+		assert.NoError(t, mock.ExpectationsWereMet(), "There were unfulfilled expectations")
+	})
+
+	// t.Run("GetUser_NotFound", func(t *testing.T) {
+	// 	userId := int64(2)
+
+	// 	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "user" WHERE id = ? ORDER BY "user"."id" LIMIT 1`)).
+	// 		WithArgs(userId).
+	// 		WillReturnError(gorm.ErrRecordNotFound)
+
+	// 	user := &domain.User{}
+	// 	err = repo.GetUser(userId, user)
+	// 	if err == nil {
+	// 		t.Errorf("expected error but got none")
+	// 	}
+
+	// 	if err != gorm.ErrRecordNotFound {
+	// 		t.Errorf("expected error to be %s, but got %s", gorm.ErrRecordNotFound, err)
+	// 	}
+
+	// 	if err := mock.ExpectationsWereMet(); err != nil {
+	// 		t.Errorf("there were unfulfilled expectations: %s", err)
+	// 	}
+	// })
+
+	// t.Run("GetUser_DBError", func(t *testing.T) {
+	// 	userId := int64(3)
+
+	// 	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "user" WHERE id = ? ORDER BY "user"."id" LIMIT 1`)).
+	// 		WithArgs(userId).
+	// 		WillReturnError(gorm.ErrInvalidDB)
+
+	// 	user := &domain.User{}
+	// 	err = repo.GetUser(userId, user)
+	// 	if err == nil {
+	// 		t.Errorf("expected error but got none")
+	// 	}
+
+	// 	if err != gorm.ErrInvalidDB {
+	// 		t.Errorf("expected error to be %s, but got %s", gorm.ErrInvalidDB, err)
+	// 	}
+
+	// 	if err := mock.ExpectationsWereMet(); err != nil {
+	// 		t.Errorf("there were unfulfilled expectations: %s", err)
+	// 	}
+	// })
+}
--- go.mod
+++ go.mod
@@ -7,22 +7,19 @@ require (
 	github.com/getkin/kin-openapi v0.128.0
 	github.com/labstack/echo/v4 v4.13.3
 	github.com/oapi-codegen/runtime v1.1.1
+	github.com/stretchr/testify v1.10.0
 	gorm.io/driver/mysql v1.5.7
-	gorm.io/driver/postgres v1.5.11
 	gorm.io/gorm v1.25.12
 )
 
 require (
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/invopop/yaml v0.3.1 // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.5.5 // indirect
-	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
@@ -32,11 +29,11 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/perimeterx/marshmallow v1.1.5 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/net v0.33.0 // indirect
-	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	golang.org/x/time v0.8.0 // indirect
